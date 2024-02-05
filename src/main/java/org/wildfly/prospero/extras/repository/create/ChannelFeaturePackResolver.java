9abdd5b965db00c45d162913d2b117f14e5194a5
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2024-02-05 15:53:02 +0000

    Remove unused import

--- src/main/java/org/wildfly/prospero/extras/repository/create/ChannelFeaturePackResolver.java
+++ src/main/java/org/wildfly/prospero/extras/repository/create/ChannelFeaturePackResolver.java
@@ -25,7 +25,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.zip.ZipException;
 
 class ChannelFeaturePackResolver {
     private static final Logger LOG = Logger.getLogger(DownloadRepositoryCommand.class);
